1ea5ed6f39e29ff914aa9f2bfb7b7f270edb1f12
Skirtle <53882475+Skirtle@users.noreply.github.com>
2020-07-08 12:58:55 -0400

    Created

---
+++ cdaproject.c
@@ -0,0 +1,14 @@
+//Headers
+#include <stdio.h>
+#include <stdlib.h>
+
+//Main method
+int main(int argc, char *argv[]) {
+    FILE* ipf = fopen(argv[0], "r"); //Opens the command line text file given
+    if (ipf == NULL) { //If the file name is wrong, or file does not exist, return 1 and exit
+        printf("Filename not valid. FILE* ipf is NULL");
+        return 1;
+    }
+
+    return 0;
+}
