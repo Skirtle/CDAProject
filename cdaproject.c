7a29f005963a6d07f1c9adb295cd5e35aa6479bf
Skirtle <53882475+Skirtle@users.noreply.github.com>
2020-07-08 22:14:21 -0400

    Added helpful .txt file
    Text file has architecture information

--- cdaproject.c
+++ cdaproject.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Struct(s)
+typedef struct {
+    int op, r1, r2, r3;
+} Instruction;
+
+
 //Main method
 int main(int argc, char *argv[]) {
     FILE* ipf = fopen(argv[0], "r"); //Opens the command line text file given
     if (ipf == NULL) { //If the file name is wrong, or file does not exist, return 1 and exit
         printf("Filename not valid. FILE* ipf is NULL");
-        return 2;
+        return 1;
     }
 
     return 0;
