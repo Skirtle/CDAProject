64a74ac8426f2e1a54115d2f3537ed0096f94bca
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-07-08 21:38:38 -0400

    Update cdaproject.c

--- cdaproject.c
+++ cdaproject.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     FILE* ipf = fopen(argv[0], "r"); //Opens the command line text file given
     if (ipf == NULL) { //If the file name is wrong, or file does not exist, return 1 and exit
         printf("Filename not valid. FILE* ipf is NULL");
-        return 1;
+        return 2;
     }
 
     return 0;
