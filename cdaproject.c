a42920e871d9856111e7af72c302d8a44e9aa5ef
Skirtle <53882475+Skirtle@users.noreply.github.com>
2020-07-14 13:56:43 -0400

    Update cdaproject.c

--- cdaproject.c
+++ cdaproject.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i < programLength; i++) {
         switch(IM[i].op) {
             case 0:
-
+                DM[IM[i].r2] = IM[i].r0;
                 break;
             case 1:
 
