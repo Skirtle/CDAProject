1b599cd6717321774e4f696799f40688b2935dc5
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-07-14 13:52:11 -0400

    Update cdaproject.c

--- cdaproject.c
+++ cdaproject.c
@@ -35,7 +35,32 @@ int main(int argc, char *argv[]) {
     int RF[8];
     
     for(int i = 0; i < programLength; i++) {
-        
+        switch(IM[i].op) {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+        }
     }
 
     //Finished, free and close pointers and files. End.
