581557fc95a6af6cf0a537cfc756f116cc40d923
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2020-07-17 21:00:33 -0400

    Update main.c

--- main.c
+++ main.c
@@ -336,37 +336,14 @@ Instruction* getFileInfo(FILE* file, int *len) {
     length = count/4;
     *len = length;
     Instruction* tempProgram = (Instruction*) calloc(length, sizeof(Instruction));
-    //Instruction tempProgram[MAX_PROGRAM_SIZE];
     actualLen = length * 8; //Funky math, but this is right.
     rewind(file);
 
-    //Create new variables and reset file pointer
-    Instruction ins[length];
-    char fullString[actualLen];
-    char regStr[length*4];
-    fgets(fullString, actualLen, file);
-    rewind(file);
-
-    //Normalize string
-    j = 0;
-    for (i = 0; i < actualLen; i++) {
-        if (fullString[i] != ' ') {
-            regStr[j] = fullString[i];
-            j++;
-        }
+    Instruction temp;
+    for (int i = 0; i < length; i++) {
+        fscanf(file, "%d %d %d %d", &temp.op, &temp.r0, &temp.r1, &temp.r2);
+        tempProgram[i] = temp;
     }
 
-    //Convert normalized string to instructions
-    j = 0;
-    for (i = 0; i < 4*length; i+=4) {
-        Instruction temp;
-        temp.op = regStr[i+0] - '0';
-        temp.r0 = regStr[i+1] - '0';
-        temp.r1 = regStr[i+2] - '0';
-        temp.r2 = regStr[i+3] - '0';
-        tempProgram[j] = temp;
-        j++;
-    }
-    
     return tempProgram;
 }
